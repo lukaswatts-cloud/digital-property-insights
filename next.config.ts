8ae4924fbd20398f422d9a28726323b109bcb238
Lukas Watts <lukas.watts@digitalpropertyinsights.com.au>
2025-10-09 06:16:18 +0000

    This looks wrong

--- next.config.ts
+++ next.config.ts
@@ -29,7 +29,8 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https'
+        ,
         hostname: 'i.ibb.co',
         port: '',
         pathname: '/**',
